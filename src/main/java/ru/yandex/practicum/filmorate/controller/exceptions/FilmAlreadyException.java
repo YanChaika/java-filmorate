5bc299bb0906fbf69fee23b1f6851776e61a1c01
Ian <ian1990@yandex.ru>
2023-05-26 13:58:30 +0300

    "style: добавить пробел в class FilmAlreadyException"

--- src/main/java/ru/yandex/practicum/filmorate/controller/exceptions/FilmAlreadyException.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/exceptions/FilmAlreadyException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.controller.exceptions;
 
-public class FilmAlreadyException extends RuntimeException{
+public class FilmAlreadyException extends RuntimeException {
 
     public FilmAlreadyException(final String message) {
         super(message);
