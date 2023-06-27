91a6f431daa421eea15d313481e011854bf8b251
Ian <ian1990@yandex.ru>
2023-06-27 09:25:22 +0400

    feat: добавить тесты FilmorateApplicationTest

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.*;
+import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.film.genres.GenreDbStorage;
+import ru.yandex.practicum.filmorate.storage.film.genres.GenresDbStorage;
+import ru.yandex.practicum.filmorate.storage.film.likes.LikesDbStorage;
+import ru.yandex.practicum.filmorate.storage.film.mpa.MpaDbStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+import ru.yandex.practicum.filmorate.storage.user.friends.FriendDbStorage;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,17 +26,112 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class FilmorateApplicationTests {
 
 	private final UserDbStorage userStorage;
+	private final GenreDbStorage genreDbStorage;
+	private final GenresDbStorage genresDbStorage;
+	private final LikesDbStorage likesDbStorage;
+	private final MpaDbStorage mpaDbStorage;
+	private final FriendDbStorage friendDbStorage;
+	private final FilmDbStorage filmDbStorage;
 
 	@Test
-	public void testFindUserById() {
+	public void testGetAllGenre() {
 
-		Optional<User> userOptional = userStorage.getById(1);
+		List<Genre> genresToCheck = genreDbStorage.getAll();
 
-		assertThat(userOptional)
+		assertThat(genresToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testFindGenreById() {
+
+		Optional<Genre> genreToCheck = genreDbStorage.getGenreById(1);
+
+		assertThat(genreToCheck)
 				.isPresent()
-				.hasValueSatisfying(user ->
-						assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+				.hasValueSatisfying(genre ->
+						assertThat(genre).hasFieldOrPropertyWithValue("id", 1)
 				);
 	}
 
+	@Test
+	public void testFindAllGenres() {
+
+		List<FilmByGenres> genresToCheck = genresDbStorage.getAll();
+
+		assertThat(genresToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetAllLikes() {
+
+		List<Like> likesToCheck = likesDbStorage.getAll();
+
+		assertThat(likesToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetSortedLikes() {
+
+		Set<Like> sortedLikes = likesDbStorage.getSortedFilms();
+
+		assertThat(sortedLikes)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetAllMPA() {
+
+		List<FilmMPA> mpaToCheck = mpaDbStorage.getAllMpa();
+
+		assertThat(mpaToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetMpaById() {
+
+		FilmMPA filmMpaToCheck = mpaDbStorage.getMpaById(1);
+
+		assertThat(filmMpaToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetAllFriends() {
+
+		List<Friend> friendsToCheck = friendDbStorage.getAll();
+
+		assertThat(friendsToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetFriendsId() {
+
+		List<Friend> friendsToCheck = friendDbStorage.getAllById(1);
+
+		assertThat(friendsToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetAllFilms() {
+
+		List<Film> filmsToCheck = filmDbStorage.getAll();
+
+		assertThat(filmsToCheck)
+				.isNotNull();
+	}
+
+	@Test
+	public void testGetSortedFilms() {
+
+		List<Film> filmsSortedToCheck = filmDbStorage.getSortedFilms();
+
+		assertThat(filmsSortedToCheck)
+				.isNotNull();
+	}
 }
