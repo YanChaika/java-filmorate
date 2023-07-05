c2e69c63ec72f242cc9fb26f9538350f03c1dfbd
bolshovya <bolshov.yv@yandex.ru>
2023-07-05 03:08:29 +0300

    style: убрал лишнее

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,18 +1,12 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.controller.exceptions.IncorrectIdException;
 import ru.yandex.practicum.filmorate.controller.exceptions.ValidationException;
-import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-import ru.yandex.practicum.filmorate.storage.user.feeds.FeedStorage;
-import ru.yandex.practicum.filmorate.storage.user.feeds.eventType.EventTypeStorage;
-import ru.yandex.practicum.filmorate.storage.user.feeds.operation.OperationStorage;
 import ru.yandex.practicum.filmorate.storage.user.friends.FriendStorage;
-
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -26,9 +20,6 @@ public class UserService {
     @Autowired
     private final UserStorage userStorage;
     private final FriendStorage friendStorage;
-    private final FeedStorage feedStorage;
-    private final EventTypeStorage eventTypeStorage;
-    private final OperationStorage operationStorage;
 
     public void addFriends(Integer id, Integer friendId) {
         friendStorage.create(getById(id), getById(friendId));
@@ -113,12 +104,4 @@ public class UserService {
         return userStorage.getById(id).get();
     }
 
-    public List<Event> findFeedsById(int id) {
-        List<Event> feeds = feedStorage.findEventsByUserId(id);
-        for (Event event : feeds) {
-            event.setEventType(eventTypeStorage.findById(event.getEventType().getId()).get());
-            event.setOperation(operationStorage.findById(event.getOperation().getId()).get());
-        }
-        return feeds;
-    }
 }
