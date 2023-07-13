39f93746e0bafc3856e6865dc558cc39be46262a
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-07-13 08:15:19 +0400

    Update and rename IncorrecIdReviewException.java to IncorrectIdReviewException.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/exceptions/IncorrecIdReviewException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.controller.exceptions;
-
-public class IncorrecIdReviewException extends RuntimeException {
-    public IncorrecIdReviewException(String message) {
-        super(message);
-    }
-}
