033ca6342cecebdc3cd5d64c0713a9dd6c292fe4
Ruslan103 <117119513+Ruslan103@users.noreply.github.com>
2023-07-13 08:19:54 +0400

    Update ErrorHandler.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -29,7 +29,7 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIncorrectReviewException(final IncorrecIdReviewException e) {
+    public ErrorResponse handleIncorrectIdReviewException(final IncorrectIdReviewException e) {
         return new ErrorResponse(e.getMessage());
     }
 }
